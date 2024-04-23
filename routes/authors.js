4e7bc21921ed5f8f2276e6886a5bcd12b643ec02
jochenruland <jochenhgruland@gmail.com>
2024-04-23 08:57:53 +0200

    ADD basic mvc setup

---
+++ routes/authors.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const Author = require('../models/author');
+
+// All author route
+router.get('/', (req, res) => {
+    res.render('authors/index')
+})
+
+// New author route
+router.get('/new', (req, res) => {
+    res.render('authors/new', { author: new Author() })
+
+})
+
+//Create author route
+router.post('/', (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+/* 
+    author.save((err, newAuthor) => {
+        if (err) {
+            res.render('authors/new', {
+                author: author,
+                errorMessage: "Error creating author"
+            })
+        } else {
+            // res.redirect(`authors/$(newAuthor.id)`)
+            res.redirect('authors')
+        }
+
+    }) */
+
+    res.send(req.body.name)
+})
+module.exports = router;
--- server.js
+++ server.js
@@ -8,15 +8,19 @@ if (process.env.NODE_ENV !== 'prodction') {
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRoute = require('./routes/index');
+const authorRoute = require('./routes/authors');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname +'/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 
+// Database connection
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true});
 const db = mongoose.connection;
@@ -25,5 +29,6 @@ db.once('open', () => console.log('Connected to mongoose'));
 
 
 app.use('/', indexRoute);
+app.use('/authors', authorRoute);
 
 app.listen(process.env.PORT || 3000);
